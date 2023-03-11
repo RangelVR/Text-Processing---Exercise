fea70369b775d4a56b9a557a98c32f761c9fa0ad
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 15:37:27 +0200

    Add files via upload

---
+++ 04. Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿string input = Console.ReadLine();
+
+string code = string.Empty;
+
+foreach (var ch in input)
+{
+    code += (char)(ch + 3);
+}
+
+Console.WriteLine(code);
+
+//--------------------------------------------------------
+//using System.Text;
+
+//string input = Console.ReadLine();
+
+//StringBuilder sb = new StringBuilder();
+
+//foreach (var ch in input)
+//{
+//    sb.Append((char)(ch + 3));
+//}
+
+//Console.WriteLine(sb.ToString());
