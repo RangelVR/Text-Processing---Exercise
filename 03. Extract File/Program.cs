5da008d1da5f8b0bdcdc6873c0c2d593bd84b5bc
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 15:03:06 +0200

    Add files via upload

---
+++ 03. Extract File/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] file = Console.ReadLine().Split("\\");
+
+string fileNameAndExtension = file.Last();
+string nameFile = fileNameAndExtension.Split(".").First();
+string extension = fileNameAndExtension.Split(".").Last();
+
+Console.WriteLine($"File name: {nameFile}");
+Console.WriteLine($"File extension: {extension}");
