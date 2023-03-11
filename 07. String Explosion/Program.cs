adca5b34ada79be6391b25805157c64beb9ede2b
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-12 00:10:10 +0200

    Add files via upload

---
+++ 07. String Explosion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+
+StringBuilder result = new StringBuilder();
+
+int bombPower = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    char ch = input[i];
+
+    if (ch == '>')
+    {
+        result.Append(ch);
+        int currBombPower = input[i + 1] - '0';
+        bombPower += currBombPower;
+    }
+    else
+    {
+        if (bombPower > 0)
+        {
+            bombPower--;
+        }
+        else
+        {
+            result.Append(ch);
+        }
+    }
+}
+
+Console.WriteLine(result);
