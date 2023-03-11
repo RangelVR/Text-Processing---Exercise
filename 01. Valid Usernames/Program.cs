6b31b86ae5def97076e5f129d236d4438596a44c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 13:24:34 +0200

    Add files via upload

---
+++ 01. Valid Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] passwords = Console.ReadLine().Split(", ");
+
+foreach (var pass in passwords)
+{
+    bool isValid = true;
+
+	if (pass.Length >= 3 && pass.Length <= 16 )
+	{
+        foreach (var ch in pass)
+        {
+            if (!char.IsLetterOrDigit(ch)  && ch != '_' && ch != '-')
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid)
+        {
+            Console.WriteLine(pass);
+        }
+    }
+}
