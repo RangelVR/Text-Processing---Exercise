09243e89aef736877f89be9cf835f9399273e90c
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 13:27:57 +0200

    Update Program.cs

--- 01. Valid Usernames/Program.cs
+++ 01. Valid Usernames/Program.cs
@@ -2,22 +2,22 @@
 
 foreach (var pass in passwords)
 {
-    bool isValid = true;
+        bool isValid = true;
 
 	if (pass.Length >= 3 && pass.Length <= 16 )
 	{
-        foreach (var ch in pass)
-        {
-            if (!char.IsLetterOrDigit(ch)  && ch != '_' && ch != '-')
-            {
-                isValid = false;
-                break;
-            }
-        }
+		foreach (var ch in pass)
+		{
+		    if (!char.IsLetterOrDigit(ch)  && ch != '_' && ch != '-')
+		    {
+			isValid = false;
+			break;
+		    }
+		}
 
-        if (isValid)
-        {
-            Console.WriteLine(pass);
-        }
-    }
+		if (isValid)
+		{
+		    Console.WriteLine(pass);
+		}
+	}
 }
