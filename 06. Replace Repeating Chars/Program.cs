2872a63ab243d0617dd8127d83441d784632133a
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 22:28:50 +0200

    Add files via upload

---
+++ 06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string letters = Console.ReadLine();
+
+for (int i = 0; i < letters.Length; i++)
+{
+    for (int k = i + 1; k <letters.Length; k++)
+    {
+        if (letters[i] == letters[k])
+        {
+            letters = letters.Remove(i, 1);
+            k--;
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+Console.WriteLine(letters);
