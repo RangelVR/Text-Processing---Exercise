57313d4396bf393eb59bce9fec73329e45c94b0d
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 21:14:09 +0200

    Add files via upload

---
+++ 05. Multiply Big Number/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string digits = Console.ReadLine();
+int multiplicator = int.Parse(Console.ReadLine());
+
+if (multiplicator == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+List<int> result = new List<int>();
+int remainder = 0;
+
+for (int i = digits.Length - 1; i >= 0; i--)
+{
+    int lastDigit = digits[i] - '0';
+    int multiplication = lastDigit * multiplicator;
+    multiplication += remainder;
+
+    result.Insert(0, multiplication % 10);
+
+    remainder = multiplication / 10;
+}
+
+if (remainder > 0)
+{
+    result.Insert(0, remainder);
+}
+
+Console.WriteLine(string.Join("", result));
+
