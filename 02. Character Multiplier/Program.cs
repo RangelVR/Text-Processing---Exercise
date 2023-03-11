a9b7fcb17800263de2b4aa46ddba235d787b40fe
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-03-11 14:34:55 +0200

    Add files via upload

---
+++ 02. Character Multiplier/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = Console.ReadLine();
+
+string firstString = input.Split().First();
+string secondString = input.Split().Last();
+
+int totalSum = 0;
+string watsLeft = string.Empty;
+
+int minLenght = Math.Min(firstString.Length, secondString.Length);
+
+for (int i = 0; i < minLenght; i++)
+{
+    totalSum += firstString[i] * secondString[i];
+}
+
+if (firstString.Length > secondString.Length)
+{
+    watsLeft = firstString.Substring(secondString.Length);
+}
+else if (secondString.Length > firstString.Length)
+{
+    watsLeft = secondString.Substring(firstString.Length);
+}
+
+foreach (var ch in watsLeft)
+{
+    totalSum += ch;
+}
+
+Console.WriteLine(totalSum);
